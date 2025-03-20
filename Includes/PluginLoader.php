944910d7a457a8c34b837ba7f026f60bf8bb851a
Ian Tumulak <edden87@gmail.com>
2025-03-21 02:24:30 +0800

    Initialized

---
+++ Includes/PluginLoader.php
@@ -0,0 +1,17 @@
+<?php
+namespace TMT\GHL\Includes;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly.
+
+class PluginLoader {
+    public function __construct() {
+        $this->load_dependencies();
+        $this->initialize_components();
+    }
+
+    private function load_dependencies() {}
+
+    private function initialize_components() {}
+}
---
+++ tmt-hmg.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Houston Medical Group
+ * Description: 
+ * Version: 1.0
+ * Author: TMT
+ * License: GPL2
+ * Text Domain: tmt-hmg
+ * 
+ * @package tmt-hmg
+ */
+
+use TMT\GHL\Includes\PluginLoader;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+define( 'TMT_HMG_PATH', plugin_dir_path( __FILE__ ) );
+define( 'TMT_HMG_URL', plugin_dir_url( __FILE__ ) );
+
+require_once TMT_HMG_PATH . '/vendor/autoload.php';
+require_once TMT_HMG_PATH . 'Includes/plugin-loader.php';
+
+new PluginLoader();
