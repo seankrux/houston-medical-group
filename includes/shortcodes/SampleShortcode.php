4039099b4bda5edd6f2c326c9e28a7e1e03ba1ee
Ian Tumulak <edden87@gmail.com>
2025-03-25 02:12:41 +0800

    Changed filenames to be capitalized.

---
+++ includes/shortcodes/SampleShortcode.php
@@ -0,0 +1,18 @@
+<?php
+namespace TMT\HMG\Includes\Shortcodes;
+
+use TMT\HMG\Includes\Interface\Shortcode;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+} // Exit if accessed directly
+
+class SampleShortcode implements Shortcode {
+    public function render( array $atts ): void {
+
+    }
+
+    public function scripts(): void {
+
+    }
+}
