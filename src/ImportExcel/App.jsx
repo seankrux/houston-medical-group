b274200c8472e5ab840c000ee8b0d10b7fc80e81
Ian Tumulak <edden87@gmail.com>
2025-03-26 03:10:53 +0800

    Initially built react dropzone.

---
+++ src/ImportExcel/App.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { useDropzone } from "react-dropzone";
+
+const App = () => {
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: {
+            "text/csv": [".csv"],
+            "application/vnd.ms-excel": [".xls"],
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": [".xlsx"],
+            "application/vnd.oasis.opendocument.spreadsheet": [".ods"]
+        },
+        onDrop: acceptedFiles => {
+            console.log(acceptedFiles);
+        },
+        multiple: false
+    });
+
+    return (
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag and drop some files here, or click to select files</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const defaults = require('@wordpress/scripts/config/webpack.config');
+
+module.exports = {
+  ...defaults,
+  entry: {
+    'import-excel': './src/ImportExcel/index.js',
+  },
+  output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].js'
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    '@wordpress/element': 'wp.element'
+  },
+};
